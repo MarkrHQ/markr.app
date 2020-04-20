983eb124f1bfec966a3efc94ec1b083b2b90db5a
Bilal Budhani <bilalbudhani@gmail.com>
2020-04-20 16:34:19 +0530

    Updates design and add twitter link

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,16 @@ class HomePage extends Component {
   render() {
     return (
       <>
-        <main className="bg-white container flex justify-center flex-col items-center mt-10 mb-4 py-8 mx-4 px-2 lg:px-0 lg:mx-auto rounded border shadow">
+        <main className="container flex justify-center flex-col items-center mt-10 mb-4 py-8 mx-4 px-2 lg:px-0 lg:mx-auto">
           <Logo />
           <h1 className="font-black text-3xl lg:text-4xl">Markr.app</h1>
           <p className="text-lg pb-8">
             Markr is a browser extension which lets you highlight and capture content on the web like a marker.
-        </p>
-          <a href="mailto:bilal@markr.app?subject=Request for early access" className="px-4 py-2 text-lg bg-yellow-200 font-bold border border-yellow-300 hover:border-yellow-400 rounded">
+          </p>
+          <a href="mailto:bilal@markr.app?subject=Request for early access" className="px-4 py-2 mb-2 text-lg bg-yellow-200 font-bold border border-yellow-300 hover:border-yellow-400 rounded hover:shadow-lg">
             Request Early Access
-        </a>
+          </a>
+        <a href="https://twitter.com/markr_app" target="_blank" className="hover:underline">Follow us on Twitter @markr_app</a>
         </main>
         <footer className="container mx-auto flex justify-center">
           <p className="text-gray-800">
